548a56f174fee80184f72d57686aee580866fc1f
Krzysztof Tomaszewski <drteski@gmail.com>
2023-09-01 12:15:43 +0200

    initial

--- components/layout/WrapperProviders.jsx
+++ components/layout/WrapperProviders.jsx
@@ -1,6 +1,13 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: true,
+      retryDelay: 1000,
+    },
+  },
+});
 const WrapperProviders = ({ children }) => {
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
