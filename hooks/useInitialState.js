2d0143068121f3ae200c341b9b30ca72965662ca
Krzysztof Tomaszewski <drteski@gmail.com>
2024-02-15 12:37:48 +0100

    reduce error fix

--- hooks/useInitialState.js
+++ hooks/useInitialState.js
@@ -1,16 +1,18 @@
 "use client";
 
 import { isToday, parseISO } from "date-fns";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const useInitialState = (data, countryId, accountId) => {
-  const [state, setState] = useState(() => {
-    return data
+  const [state, setState] = useState([]);
+  useEffect(() => {
+    const initData = data
       .filter((country) => country.countryId === countryId)
       .filter((account) => account.accountId === accountId)
       .map((date) => (isToday(parseISO(date.createdAt)) ? date.count : 0))
       .reduce((acc, curr) => acc + curr, 0);
-  });
+    setState(initData);
+  }, [data, countryId, accountId]);
   return [state, setState];
 };
 
