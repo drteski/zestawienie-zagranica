a965397c735e1790d52af5673f6379647b613f7f
Krzysztof Tomaszewski <drteski@gmail.com>
2024-02-16 09:45:47 +0100

    reduce error fix

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+
+export const dynamic = "force_dynamic";
 export const authOptions = {
   providers: [
     CredentialsProvider({
